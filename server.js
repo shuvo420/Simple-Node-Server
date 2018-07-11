7ca39dc056444dbbc31a6e10c43d31a5eee4d65c
Al Mahmud <mdalmahmud0183@gmail.com>
2018-07-11 03:19:22 +0000

    classwork_5_6

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var express= require('express');
+var app= express();
+var server= http.server(app);
+app.get('/', function(req,res){
+    res.sendFile(_dirname+'/index.html');
+});
+
+/* var fs = require('fs');
+  var server = http.createServer(function(req, res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('index.html', function(err, data){
+        if(err){
+            return console.log(err);
+            }
+            
+    
+    res.end(data);
+  });
+  });
+  */
+  server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+  });

8f7f5dc8b9dfe985554c4d0a42e79e8c3461d10f
Al Mahmud <mdalmahmud0183@gmail.com>
2018-07-18 17:37:30 +0000

    classwork 7

---
+++ route.js
@@ -0,0 +1,1 @@
+router.get('/genre/create', genre_controller.genre_create_get);
--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 var http = require('http');
-var express= require('express');
-var app= express();
-var server= http.server(app);
-app.get('/', function(req,res){
-    res.sendFile(_dirname+'/index.html');
+  var express = require('express');
+  var app = express();
+  var server = http.Server(app);
+  
+  var bodyParser = require('body-parser'); 
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true}));
+  
+
+app.get('/', function (req, res) {
+     res.sendFile(__dirname+'/index.html');
 });
 
-/* var fs = require('fs');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html', function(err, data){
-        if(err){
-            return console.log(err);
-            }
-            
+app.get('/about',function(rep,res){
+  res.sendFile(__dirname+'/about.html');
+})
+
+app.get('/form', function (req, res) {
+     res.sendFile(__dirname+'/form.html');
+});
+
+//app.post('/form', ,urlencodedParser, function (req, res) {
     
-    res.end(data);
-  });
-  });
-  */
-  server.listen(process.env.PORT, process.env.IP, function(){
+app.post('/submit_user', function (req, res) {
+  //  console.log(req.body.name);
+    //console.log(req.body.email);
+    console.log(req.body);
+    
+    // res.sendFile(__dirname+'/form.html');
+});
+
+
+
+
+// Print URL for accessing server
+ server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });

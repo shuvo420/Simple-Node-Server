3f970011b25c83734edcd16146175a28caa7fc3f
Al Mahmud <mdalmahmud0183@gmail.com>
2018-07-25 03:31:21 +0000

    classwork 9

--- server.js
+++ server.js
@@ -1,39 +1,28 @@
 var http = require('http');
-  var express = require('express');
-  var app = express();
-  var server = http.Server(app);
-  
-  var bodyParser = require('body-parser'); 
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended:true}));
-  
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', function (req, res) {
-     res.sendFile(__dirname+'/index.html');
+var server = http.Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+'/index.html');
 });
 
-app.get('/about',function(rep,res){
+app.get('/form', function(req, res){
+  res.sendFile(__dirname+'/form.html');
+});
+
+app.post('/submit_user', function(req, res){
+  console.log(req.body);
+});
+
+app.get('/system/about', function(req, res){
   res.sendFile(__dirname+'/about.html');
-})
-
-app.get('/form', function (req, res) {
-     res.sendFile(__dirname+'/form.html');
 });
 
-//app.post('/form', ,urlencodedParser, function (req, res) {
-    
-app.post('/submit_user', function (req, res) {
-  //  console.log(req.body.name);
-    //console.log(req.body.email);
-    console.log(req.body);
-    
-    // res.sendFile(__dirname+'/form.html');
-});
-
-
-
-
-// Print URL for accessing server
- server.listen(process.env.PORT, process.env.IP, function(){
+server.listen(process.env.PORT || 3000, process.env.IP || 'localhost', function(){
     console.log('Server running');
-  });
+})

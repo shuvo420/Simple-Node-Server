bfa6708a1bda9080e52d4f2f9473ae4828e67c6c
Al Mahmud <mdalmahmud0183@gmail.com>
2018-07-30 03:17:01 +0000

    classwork_9

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 var server = http.Server(app);
+var mongo=require('mongodb');
+//for local
+//var db, uri="mongo://localhost:27017";
+//for c9
+var db, uri="mongodb://"+process.env.IP+":27017"
+
+mongo.MongoClient.connect(uri,
+{useNewUrlparser: true},
+function(err, client){
+    if(err){
+        console.log("could not connect to mongodb");
+    }
+    else{
+        db=client.db('simple-node');
+    }
+}
+);
+var save= function(form_data){
+    db.createCollection('users', function(err, collection){});
+    var collection =db.collection('users');
+    collection.save(form_data);
+}
 
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/index.html');
@@ -17,6 +39,8 @@ app.get('/form', function(req, res){
 
 app.post('/submit_user', function(req, res){
   console.log(req.body);
+  save(req.body);
+  res.status()
 });
 
 app.get('/system/about', function(req, res){
